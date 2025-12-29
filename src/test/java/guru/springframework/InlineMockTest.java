7a9774db5c2b9551b1cef0e6678eacc04800d472
Pavel Glukhov <pvglukhov@gmail.com>
2025-12-29 22:50:51 +0300

    [master] добавлено несколько тестов

--- src/test/java/guru/springframework/InlineMockTest.java
+++ src/test/java/guru/springframework/InlineMockTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
-public class InlineMockTest {
+class InlineMockTest {
     @Test
     void testInlineMock() {
         Map mapMock = mock(Map.class);
-        assertEquals(mapMock.size(), 0);
+        doReturn(2).when(mapMock).size();
+        assertEquals(mapMock.size(), 2);
     }
 }
--- src/test/java/guru/springframework/JUnitExtensionTest.java
+++ src/test/java/guru/springframework/JUnitExtensionTest.java
@@ -1,24 +1,25 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
-public class JUnitExtensionTest {
+class JUnitExtensionTest {
     @Mock
     Map<String, Object> mapMock;
 
     @Test
     void annotationMapTest() {
         assertEquals(mapMock.size(), 0);
+        doReturn(5).when(mapMock).size();
         mapMock.put("test", "success");
+        assertEquals(mapMock.size(), 5);
     }
 }
